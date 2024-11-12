f2606991757b3e781fe3db3a47b32c7fa2dcae06
MSI Shamim <64169828+msi-shamim@users.noreply.github.com>
2024-11-12 16:06:05 +0600

    Initial Commit

---
+++ ProxyApp/ProxyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxyAppApp.swift
+//  ProxyApp
+//
+//  Created by MSI Shamim on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProxyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProxyAppTests/ProxyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProxyAppTests.swift
+//  ProxyAppTests
+//
+//  Created by MSI Shamim on 12/11/24.
+//
+
+import Testing
+@testable import ProxyApp
+
+struct ProxyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
